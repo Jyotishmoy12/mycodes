f31ab962f065b4952758d4d96025a28f8e72718c
Jyotishmoy Deka <93263133+Jyotishmoy12@users.noreply.github.com>
2022-06-23 00:28:51 +0530

    Create richestcustomerwealth.cpp

---
+++ richestcustomerwealth.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int maxWealth=0;
+        int customerWealth=0;
+        for(int i=0;i<accounts.size();i++){
+            customerWealth=0;
+            for(int j=0;j<accounts[i].size();j++){
+                customerWealth+=accounts[i][j];
+            }
+            if(customerWealth>maxWealth)
+                maxWealth=customerWealth;
+        }
+        return maxWealth;
+    }
+};
